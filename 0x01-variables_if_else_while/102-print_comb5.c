dd3d78ae9450ffe178a9139a27ffaae6f93dc1bf
Oaniyi2959 <olaniyiafolayan@yahoo.com>
2023-08-11 16:34:06 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
+
 /**
- * main - program starting point
- * Return: 0 returned when success
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
+int i = 0;
+int j = 0;
 
-for (n = 0; n < 10; n++)
+while (i <= 99)
 {
-putchar((n % 10) + '0');
-if (n == 9)
-continue;
-putchar(',');
+j = i + 1;
+while (j <= 99)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+
 putchar(' ');
+
+putchar(j / 10 + '0');
+
+putchar(j % 10 + '0');
+
+if (i != 98 || j != 99)
+
+{
+
+putchar(',');
+
+putchar(' ');
+
+}
+
+j++;
+
+}
+
+i++;
 }
 putchar('\n');
 return (0);
